002e3cd78fb8e9b1ec98ef5e0ff5a9722313847f
Yong Dong <yongdong11467@gmail.com>
2018-11-02 20:22:24 -0400

    added win condition and moves

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Game {
-    char Player;
-    char Computer;
-    char[][] board = new char[][] {
+    private char Player;
+    private char Computer;
+    private char[][] board = new char[][] {
             {'_', '_', '_'},
             {'_', '_', '_'},
             {'_', '_', '_'},
@@ -19,6 +19,11 @@ public class Game {
         }
         System.out.println(" _______");
     }
+
+    public char getPlayer() {
+        return Player;
+    }
+
     public void whoGoesFirst() {
         String choice;
         do {
@@ -37,31 +42,48 @@ public class Game {
             }
         } while (!(choice.equals("1") || choice.equals("2")));
     }
-    public int[][] possMove(char player, char oppenent) {
+    public int possMove() {
         int[][] possMove = new int[3][3];
+        int count = 0;
         for (int i = 0; i < this.board.length; i++) {
             for (int j = 0; j < this.board[i].length; j++) {
                 if (this.board[i][j] == '_') {
                     possMove[i][j] = 0;
+                    count++;
                 } else {
                     possMove[i][j] = -1;
                 }
             }
         }
-        return possMove;
+        return count;
+    }
+    public void move(char player, char oppenent) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.printf("%s's move!\n", player);
+            displayBoard();
+            System.out.println("Enter Row then Column");
+            int rc = sc.nextInt();
+            if (this.board[rc / 10][rc % 10] == '_') {
+                this.board[rc / 10][rc % 10] = player;
+                break;
+            } else {
+                System.out.println("Not a valid move");
+            }
+        }
     }
     public boolean win() {
         for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == board [i][1] && board [i][1] == board[i][2]) {
+            if (board[i][0] == board [i][1] && board [i][1] == board[i][2] && board[i][0] != '_') {
                 return true;
             }
-            if (board[0][i] == board [1][i] && board [1][i] == board[2][i]) {
+            if (board[0][i] == board [1][i] && board [1][i] == board[2][i] && board[0][i] != '_') {
                 return true;
             }
-            if (board[0][0] == board [1][1] && board [1][1] == board[2][2]) {
+            if (board[0][0] == board [1][1] && board [1][1] == board[2][2] && board[0][0] != '_') {
                 return true;
             }
-            if (board[0][2] == board [1][1] && board [1][1] == board[2][0]) {
+            if (board[0][2] == board [1][1] && board [1][1] == board[2][0] && board[0][2] != '_') {
                 return true;
             }
         }
--- src/run.java
+++ src/run.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
 
 public class run {
-    public static void start() {
+    public void start() {
         Game g = new Game();
         System.out.println("               You will never be able to win this game");
         System.out.println("*** Don't even try cheating the system by messing with the original code ***");
         System.out.println("                        !!!!!THAT'S UNETHICAL!!!!!");
         g.whoGoesFirst();
-        g.displayBoard();
-        while (!g.win()) {
-
+        while (!g.win() || g.possMove() == 0) {
+            if (g.getPlayer() == 'X') {
+                g.move('X', 'O');
+            } else if (g.getPlayer() == 'O') {
+                g.move('O', 'X');
+            }
         }
-
+        g.displayBoard();
+        System.out.println("Finish");
     }
     public static void main(String[] args) {
-        start();
+        run r = new run();
+        r.start();
     }
 }
